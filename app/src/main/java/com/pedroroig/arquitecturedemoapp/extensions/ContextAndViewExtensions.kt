c1e2b47810497dedeb49711d44f7299ffaf949b7
Pedro Roig <pedro.roiggimenez@soprasteria.com>
2019-12-19 15:58:07 +0100

    add view extensions for convenience

---
+++ app/src/main/java/com/pedroroig/arquitecturedemoapp/extensions/ContextAndViewExtensions.kt
@@ -0,0 +1,22 @@
+package com.pedroroig.arquitecturedemoapp.extensions
+
+import android.content.Context
+import android.view.View
+import android.widget.Toast
+
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    visibility = View.GONE
+}
+
+fun View.invisibilize() {
+    visibility = View.INVISIBLE
+}
+
+fun Context.toast(message: String, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, length).show()
+}
