62a97035b8b097b10e0927b551763539a92c418d
Pedro Roig <pedro.roiggimenez@soprasteria.com>
2019-12-19 16:00:21 +0100

    develop render method in the activity

--- app/src/main/java/com/pedroroig/arquitecturedemoapp/LoginActivity.kt
+++ app/src/main/java/com/pedroroig/arquitecturedemoapp/LoginActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.pedroroig.arquitecturedemoapp.extensions.hide
+import com.pedroroig.arquitecturedemoapp.extensions.show
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -20,6 +23,15 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun render(viewState: LoginViewState) {
+        with(viewState) {
+            if(infoMessage != null) text_message.show()
+            else text_message.hide()
 
+            if(loginText != null) edit_text_user.show()
+            else edit_text_user.hide()
+
+            if(showPasswordField) edit_text_pass.show()
+            else edit_text_pass.hide()
+        }
     }
 }
